cead90eb18b12c341cb137a9e971335a70a8ea5c
Ivan <kiberaction@yandex.ru>
2018-02-05 03:01:47 +0300

    added "file input"

--- ctasks/ctasks.cpp
+++ ctasks/ctasks.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
+	setlocale(LC_ALL, "rus");
+	std::cout << "input file content: " << std::endl;
+	char str[100] = "";
+	std::cin >> str;
+	std::string input_string = std::string(str);
+	std::ofstream fout;
+	fout.open("hello.txt");
+	fout << input_string;
+	fout.close();
+	system("pause");
     return 0;
 }
 
